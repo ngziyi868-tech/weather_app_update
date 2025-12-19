91a2698176d88938601d85890092cfa5079f33c0
Ziyi <ngziyi868@gmail.com>
2025-12-18 19:07:32 -0700

    trying some things

--- weather-header.js
+++ weather-header.js
@@ -1,1 +1,10 @@
-`https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid={889f86ea113204ba293d53abe0617a4b}`
+const weatherURL= "https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid={889f86ea113204ba293d53abe0617a4b}"
+
+fetch(weatherURL)
+    .then(response => response.json())
+    .then(jsObject => {
+
+    });
+
+
+document.getElementById('current-temp')    
