72c9fd6314bb18601e08c059677c07e6ea8984e1
Ziyi <ngziyi868@gmail.com>
2026-01-05 16:58:14 -0700

    calculating windchill

--- weather-header.js
+++ weather-header.js
@@ -1,19 +1,41 @@
-const weatherURL= "https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid={889f86ea113204ba293d53abe0617a4b}"
+const city = "Tooele";
+const weatherURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=tooele&units=imperial&appid=889f86ea113204ba293d53abe0617a4b";
 
-fetch(weatherURL)
-    .then(response => {
-        if (!response.ok) {
-     
-        }
-    }
+async function getWeatherData() {
+  try {
+    const weatherResponse = await fetch(weatherURL);
+    const weatherData = await weatherResponse.json();
 
-    );
+    displayCurrentWeather(weatherData);
+
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error");
+  }
+}
+
+getWeatherData();
 
 
-document.getElementById('current-temp').textContent = `${temp}°F`;
-document.getElementById('current-windChill').textContent = `${windChill}°F`;
-document.getElementById('current-humid').textContent = `${humidity}%`;
-document.getElementById('current-windSpeed').textContent = `${windSpeed} mph`;
-document.getElementById('current-desc').textContent = weatherDesc;
+function displayCurrentWeather(data) {
+    const temp = data,main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    const weatherDesc = data.weather[0].description;
+    
 
-const iconURL = ``
+}
+
+
+
+document.getElementById("current-temp").textContent = `${temp}°F`;
+document.getElementById("current-windChill").textContent = `${windChill}°F`;
+document.getElementById("current-humid").textContent = `${humidity}%`;
+document.getElementById("current-windSpeed").textContent = `${windSpeed} mph`;
+document.getElementById("current-desc").textContent = weatherDesc;
+
+const iconURL = `https://openweathermap.org/img/wn/{data.weather[0].icon}@2x.png`;
+const icon = document.getElementById("current-icon");
+icon.src = iconURL;
+icon.alt = weatherDesc;
