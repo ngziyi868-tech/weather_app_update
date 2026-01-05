6d6c0d47d1866879f7f2f174f49114784099b39e
Ziyi <ngziyi868@gmail.com>
2026-01-05 16:19:58 -0700

    fixing bugs and writing javascript

--- weather-header.js
+++ weather-header.js
@@ -1,10 +1,19 @@
 const weatherURL= "https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid={889f86ea113204ba293d53abe0617a4b}"
 
 fetch(weatherURL)
-    .then(response => response.json())
-    .then(jsObject => {
+    .then(response => {
+        if (!response.ok) {
+     
+        }
+    }
 
-    });
+    );
 
 
-document.getElementById('current-temp')    
+document.getElementById('current-temp').textContent = `${temp}°F`;
+document.getElementById('current-windChill').textContent = `${windChill}°F`;
+document.getElementById('current-humid').textContent = `${humidity}%`;
+document.getElementById('current-windSpeed').textContent = `${windSpeed} mph`;
+document.getElementById('current-desc').textContent = weatherDesc;
+
+const iconURL = ``
