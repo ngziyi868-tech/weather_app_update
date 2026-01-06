e637e4011346554408d13de0b28bceee8f911e6b
Ziyi <ngziyi868@gmail.com>
2026-01-05 19:26:44 -0700

    finished updating

--- weather-header.js
+++ weather-header.js
@@ -1,20 +1,13 @@
 const city = "Tooele";
 const apiKey = "889f86ea113204ba293d53abe0617a4b";
 const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=tooele&units=imperial&appid=${apiKey}`;
-const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=tooele&units=imperial&appid=${apiKey}`;
 
 async function getWeatherData() {
   try {
     const response = await fetch(weatherURL);
-    if (!weatherResponse.ok) throw new Error("Error");
-    const weatherData = await weatherResponse.json();
+    const data = await response.json();
 
-    displayCurrentWeather(weatherData);
-
-    const forecastResponse = await fetch(forecastURL);
-    if (!forecastResponse.ok) throw new Error("Error");
-    const forecastData = await forecastResponse.json();
-    displayForecast(forecastData);
+    displayCurrentWeather(data);
 
     updateCurrentDate();
   } catch (error) {
@@ -29,7 +22,6 @@ function displayCurrentWeather(data) {
   const temp = data.main.temp;
   const humidity = data.main.humidity;
   const windSpeed = data.wind.speed;
-  const weatherDesc = data.weather[0].description;
 
   let windChill = "N/A";
   if (temp <= 50 && windSpeed > 3) {
@@ -41,42 +33,33 @@ function displayCurrentWeather(data) {
     windChill = Math.round(windChill);
   }
 
-  document.getElementById("current-temp").textContent = `${temp}°F`;
-  document.getElementById("current-windChill").textContent = `${windChill}°F`;
-  document.getElementById("current-humid").textContent = `${humidity}%`;
-  document.getElementById("current-windSpeed").textContent = `${windSpeed} mph`;
+  const tempEl = document.getElementById("current-temp");
+  if(tempEl) tempEl.textContent = `${temp}°F`;
+
+  const windChillEl = document.getElementById("current-windChill");
+  if(windChillEl)windChillEl.textContent = `${windChill}°F`;
+
+  const humidEl = document.getElementById("current-humid");
+  if(humidEl) humidEl.textContent = `${humidity}%`;
+
+  const windSpeedEl = document.getElementById("current-windSpeed");
+  if(windSpeedEl) windSpeedEl.textContent = `${windSpeed} mph`;
 
  
-  const icon = document.getElementById("current-icon");
-  icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  icon.alt = weatherDesc;
-}
+  const iconEl = document.getElementById("current-icon");
+  if(iconEl){
+  iconEl.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  iconEl.alt = "weatherDesc";
 
-function displayForecast(data) {
-  const dailyForecast = data.list.filter(item => item.dt_txt.includes("12:00:00"));
-
-  dailyForecast.slice(0,5).forEach((dayData, index) =>{
-    const temp= Math.round(dayData.main.temp);
-    const iconURL = `https://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png`;
-    const desc = dayData.weather[0].description;
-
-    const tempElement = document.getElementById(`data${index + 1}`);
-    if (tempElement) tempElement.textContent = temp;
-
-    const forecastImg = document.querySelectorAll(".flex-col img")[index];
-    if(forecastImg){
-      forecastImg.src = iconURL;
-      forecastImg.alt = desc;
-    }
-  });
+  }
 }
 
 
 function updateCurrentDate() {
   const dateElement = document.getElementById("current-date");
   if (!dateElement) return;
+
   const now = new Date();
   const options = { weekday: "long", day: "numeric", year: "numeric" };
-  const formattedDate = now.toLocaleDateString("en-US", options);
-  dateElement.textContent = `Date: ${formattedDate}`;
+  dateElement.textContent = `Date: ${now.toLocaleDateString("en-US", options)}`;
 }
