ff6f2f8ba01570250e836fe1c3949e95ff0125d5
Ziyi <ngziyi868@gmail.com>
2026-01-07 17:19:28 -0700

    forgot to declare value to linkEl

--- weather-header.js
+++ weather-header.js
@@ -2,9 +2,7 @@ const city = "Tooele";
 const apiKey = "889f86ea113204ba293d53abe0617a4b";
 const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=tooele&units=imperial&appid=${apiKey}`;
 
-
-const URL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=18abc84001204202af32ebe2354a09a3`
-
+const newsURL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=18abc84001204202af32ebe2354a09a3`;
 
 async function getWeatherData() {
   try {
@@ -38,31 +36,68 @@ function displayCurrentWeather(data) {
   }
 
   const tempEl = document.getElementById("current-temp");
-  if(tempEl) tempEl.textContent = `${temp}°F`;
+  if (tempEl) tempEl.textContent = `${temp}°F`;
 
   const windChillEl = document.getElementById("current-windChill");
-  if(windChillEl)windChillEl.textContent = `${windChill}°F`;
+  if (windChillEl) windChillEl.textContent = `${windChill}°F`;
 
   const humidEl = document.getElementById("current-humid");
-  if(humidEl) humidEl.textContent = `${humidity}%`;
+  if (humidEl) humidEl.textContent = `${humidity}%`;
 
   const windSpeedEl = document.getElementById("current-windSpeed");
-  if(windSpeedEl) windSpeedEl.textContent = `${windSpeed} mph`;
+  if (windSpeedEl) windSpeedEl.textContent = `${windSpeed} mph`;
 
- 
   const iconEl = document.getElementById("current-icon");
-  if(iconEl){
-  iconEl.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  iconEl.alt = "weatherDesc";
-
+  if (iconEl) {
+    iconEl.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    iconEl.alt = "weatherDesc";
   }
 }
 
-function displayCurrentNews(newsData){
-  if(!newsData.articles || newsData.articles.length === 0) return;
-  
-}
+function displayCurrentNews(newsData) {
+  if (!newsData.articles || newsData.articles.length === 0) return;
+  const articles = newsData.articles;
 
+  const mainArticle = articles[0];
+
+  const newsHeader = document.getElementById("header-article");
+  const headerDesc = document.getElementById("header-desc");
+  const newsImg = document.getElementById("article-image-0");
+
+  if (newsHeader) {
+    newsHeader.textContent = mainArticle.url;
+    newsHeader.href = mainArticle.url;
+    newsHeader.target = "_blank";
+  }
+
+  if (headerDesc) {
+    headerDesc.textContent = mainArticle.description || "";
+  }
+
+  if (headerImg && mainArticle.urlToImage) {
+    newsImg.src = mainArticle.urlToImage;
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    const article = articles[i];
+    if (!article) continue;
+
+    const linkEl = document.getElementById("article-link-${i}");
+    const descEl = document.getElementById("article-desc-${i}");
+    const imgEl = document.getElementById("article-img-${i}");
+
+  
+
+    if (descEl) {
+      descEl.textContent = article.description || "";
+    }
+
+
+    if (imgEl && article.urlToImage) {
+      imgEl.src = article.urlToImage;
+    }
+  }
+}
 
 function updateCurrentDate() {
   const dateElement = document.getElementById("current-date");
@@ -72,7 +107,4 @@ function updateCurrentDate() {
   const options = { weekday: "long", day: "numeric", year: "numeric" };
   dateElement.textContent = `Date: ${now.toLocaleDateString("en-US", options)}`;
 }
-
-
-const URL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=18abc84001204202af32ebe2354a09a3`
 
